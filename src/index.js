0b837c10ec03edbb67752768c305bef24c445ead
Maksim <maks.yermakou@gmail.com>
2018-09-20 13:41:13 +0300

    My solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	var stack = [];
+	var balanced = true;
+
+	for(var i = 0; i < str.length && balanced; i++){
+		var symbol = str[i];
+
+		for(var j = 0; j < bracketsConfig.length; j++){
+			if (symbol == bracketsConfig[j][0]){
+				if (symbol == bracketsConfig[j][1]){
+					if(stack.length == 0 || stack[stack.length-1] != symbol){
+						stack.push(symbol);
+						break;
+					}else{
+						stack.pop();
+						balanced = true;
+						break;
+					}
+				}
+				stack.push(symbol);
+				break;
+			}
+			else{
+				if (j + 1 != bracketsConfig.length)
+					continue;
+				if(stack.length == 0){
+					balanced = false;
+				}
+				else{
+					for(var k = 0; k < bracketsConfig.length; k++){
+						if(stack[stack.length-1] == bracketsConfig[k][0] && symbol == bracketsConfig[k][1]){
+							stack.pop();
+							balanced =true;
+							break;
+						}else{
+							balanced = false;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	if(balanced && stack.length == 0)
+		return true;
+	else
+		return false;
 }
